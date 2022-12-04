ca4c25126abbadbd5a44bab575ced37dfb3595ed
sherly santiadi <sntdshrly@gmail.com>
2022-12-04 14:14:34 +0700

    create floor

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
+// Variable
 let scene, camera, renderer;
-
+// Scene
 scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd);
-
+// Camera
 camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
 camera.rotation.y = 45 / 180 * Math.PI;
 camera.position.x = 800;
 camera.position.y = 100;
 camera.position.z = 1000;
-
+// Renderer
 renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-window.addEventListener('resize', (evt)=>{
+// Orbit Controls
+window.addEventListener('resize', (evt) => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
-controls = new THREE.OrbitControls(camera,renderer.domElement);
-
-hlight = new THREE.AmbientLight(0x404040, 100);
-scene.add(hlight);
-
-directionalLight = new THREE.DirectionalLight(0xffffff, 100);
-directionalLight.position.set(0, 1, 0);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
-light = new THREE.PointLight(0xc4c4c4, 10);
-light.position.set(0, 300, 500);
-scene.add(light);
-light2 = new THREE.PointLight(0xc4c4c4, 10);
-light2.position.set(500, 100, 0);
-scene.add(light2);
-light3 = new THREE.PointLight(0xc4c4c4, 10);
-light3.position.set(0, 100, -500);
-scene.add(light3);
-light4 = new THREE.PointLight(0xc4c4c4, 10);
-light4.position.set(-500, 300, 500);
-scene.add(light4);
-
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+// Light
+const light01 = new THREE.DirectionalLight(0x696880);
+light01.intensity = 2;
+light01.position.set(0,2,0);
+light01.castShadow = true;
+scene.add(light01);
+// hlight = new THREE.AmbientLight(0x404040, 100);
+// scene.add(hlight);
+// directionalLight = new THREE.DirectionalLight(0x696880, 100);
+// directionalLight.position.set(0, 1, 0);
+// directionalLight.castShadow = true;
+// scene.add(directionalLight);
+// light = new THREE.PointLight(0xc4c4c4, 10);
+// light.position.set(0, 300, 500);
+// scene.add(light);
+// light2 = new THREE.PointLight(0xc4c4c4, 10);
+// light2.position.set(500, 100, 0);
+// scene.add(light2);
+// light3 = new THREE.PointLight(0xc4c4c4, 10);
+// light3.position.set(0, 100, -500);
+// scene.add(light3);
+// light4 = new THREE.PointLight(0xc4c4c4, 10);
+// light4.position.set(-500, 300, 500);
+// scene.add(light4);
+// Helper
+const helper01 = new THREE.PointLightHelper(light01,200,new THREE.Color(1,0,0));
+scene.add(helper01);
+// Texture
+const grassTexture = new THREE.TextureLoader().load('assets/grass/grass1-albedo3.png');
+// Car
 let loader = new THREE.GLTFLoader();
 loader.load('assets/scene.gltf', function (gltf) {
     car = gltf.scene.children[0];
-    car.scale.set(0.5, 0.5, 0.5);
+    car.scale.set(0.25, 0.25, 0.25);
+    car.position.set(0, 0, 0);
     scene.add(gltf.scene);
-    animate();
 });
-
+// Floor
+const geo = new THREE.BoxGeometry(5000,10,5000);
+const mat = new THREE.MeshStandardMaterial({map:grassTexture});
+const floor = new THREE.Mesh(geo,mat);
+floor.position.set(0,-100,0);
+floor.receiveShadow = true;
+scene.add(floor);
+// Animate
 function animate() {
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
+animate();
