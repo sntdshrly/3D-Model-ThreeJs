d136149611660f91da2f93d93acc78916ecd14cd
sherly santiadi <sntdshrly@gmail.com>
2022-12-09 09:05:26 +0700

    Change camera

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let speed = 5;
 scene = new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd);
 // Camera
-camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 7000);
 camera.rotation.y = 45 / 180 * Math.PI;
 camera.position.x = 800;
 camera.position.y = 100;
-camera.position.z = 1000;
+camera.position.z = 1500;
 // Renderer
 renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
